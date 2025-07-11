b33b7d3feb67e9053785671701ccf62fc874aabc
hyunwoo <hyunwoo@aah-e.net>
2025-07-11 15:54:09 +0200

    Changed "Analytics.tsx" html architecture.

--- frontend/src/pages/Analytics.tsx
+++ frontend/src/pages/Analytics.tsx
@@ -20,7 +20,7 @@ const Analytics: React.FC = () => {
   const inputRef = useRef<HTMLInputElement>(null); // input box
 
   /*******************************************************
-   * methods
+   * handlers
    */
   const validateUrl = (value: string): boolean => {
     setIsValidUrl(true);
@@ -58,7 +58,7 @@ const Analytics: React.FC = () => {
    * lifecycle hooks
    */
   useEffect(() => {
-    // Focus the input element when the component mounts
+    // Focus the input
     if (inputRef.current) {
       inputRef.current.focus();
     }
@@ -74,52 +74,60 @@ const Analytics: React.FC = () => {
    * render
    */
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-gray-800">Analytics</h1>
-      <p className="mt-2 text-lg text-gray-600">
-        Enter the URL for page analysis
-      </p>
-      <div className="mt-4 flex items-center">
-        <input
-          type="text"
-          value={url}
-          disabled={pending}
-          onChange={(e) => {
-            setUrl(e.target.value);
-            setIsValidUrl(true);
-            setValidationMessage(null);
-          }}
-          onBlur={handleBlur} // Add onBlur event handler
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleSendClick();
-            }
-          }}
-          placeholder="https://url"
-          className={`border p-2 rounded-md w-full max-w-md placeholder-gray-300 ${
-            !isValidUrl ? "border-red-500" : "border-gray-500"
-          } ${
-            pending
-              ? "bg-gray-100 cursor-not-allowed text-gray-500"
-              : "bg-white text-gray-800"
-          }
-          }`}
-          ref={inputRef} // Attach the ref to the input element
-        />
-        <button
-          onClick={handleSendClick}
-          className="ml-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md w-22 h-10 flex items-center justify-center"
-        >
-          {pending ? (
-            <FaSpinner className="animate-spin" size={18} />
-          ) : (
-            <span>Send</span>
-          )}
-        </button>
+    <div className="container mx-auto p-4">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl font-bold text-gray-800">Analytics</h1>
       </div>
-      {validationMessage && (
-        <p className="text-red-500 text-sm mt-2">{validationMessage}</p>
-      )}
+      <div className="mt-4 p-4 w-full bg-white shadow-md rounded-lg overflow-hidden">
+        <div className="mb-3 ">
+          <div className="relative">
+            <p className="my-2 w-full text-lg text-gray-600">
+              Enter the URL for page analysis
+            </p>
+            <div className="flex items-center">
+              <input
+                type="text"
+                value={url}
+                disabled={pending}
+                onChange={(e) => {
+                  setUrl(e.target.value);
+                  setIsValidUrl(true);
+                  setValidationMessage(null);
+                }}
+                onBlur={handleBlur} // Add onBlur event handler
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleSendClick();
+                  }
+                }}
+                placeholder="https://url"
+                className={`border p-2 rounded-md w-full max-w-md placeholder-gray-300 ${
+                  !isValidUrl ? "border-red-500" : "border-gray-500"
+                } ${
+                  pending
+                    ? "bg-gray-100 cursor-not-allowed text-gray-500"
+                    : "bg-white text-gray-800"
+                }
+          }`}
+                ref={inputRef} // Attach the ref to the input element
+              />
+              <button
+                onClick={handleSendClick}
+                className="ml-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md w-22 h-10 flex items-center justify-center"
+              >
+                {pending ? (
+                  <FaSpinner className="animate-spin" size={18} />
+                ) : (
+                  <span>Send</span>
+                )}
+              </button>
+            </div>
+            {validationMessage && (
+              <p className="text-red-500 text-sm mt-2">{validationMessage}</p>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
