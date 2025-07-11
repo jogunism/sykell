08941718b377c720eb085dcb19b69d93e6f54cd9
hyunwoo <hyunwoo@aah-e.net>
2025-07-11 15:53:14 +0200

    Changed API path : added prefix "/api"

--- frontend/src/lib/http.ts
+++ frontend/src/lib/http.ts
@@ -3,8 +3,11 @@ import axios from "axios";
 const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 const BASE_API_TOKEN = import.meta.env.VITE_BASE_API_TOKEN;
 
+// console.log(`base url: ${BASE_API_URL}`);
+// console.log(`token : ${BASE_API_TOKEN}`);
+
 const http = axios.create({
-  baseURL: BASE_API_URL,
+  baseURL: `${BASE_API_URL}/api`,
   headers: {
     "Content-Type": "application/json",
     Authorization: BASE_API_TOKEN,
